f22115c1727d62551f7324e2211a15b0bc185e27
Micha <Micha@DESKTOP-5SFL2D5>
2016-12-14 17:42:30 +0100

    Second Commit

--- jaxb5/src/test/java/com/cedros/jaxb/test/MyActivitiTest.java
+++ jaxb5/src/test/java/com/cedros/jaxb/test/MyActivitiTest.java
@@ -16,31 +16,31 @@ import org.junit.Test;
 public class MyActivitiTest {
 
 	@Rule
-	public ActivitiRule activitiRule = new ActivitiRule();
+	public ActivitiRule activitiRules = new ActivitiRule();
 
 	@Test
 	@Deployment(resources = { "MyProcess.bpmn20.xml" })
 	public void myProcessTest() {
-		ProcessInstance processInstance = this.activitiRule.getRuntimeService()
+		ProcessInstance processInstance = this.activitiRules.getRuntimeService()
 				.startProcessInstanceByKey("financialReport");
 		assertNotNull(processInstance);
-		Task task = this.activitiRule.getTaskService().createTaskQuery().singleResult();
+		Task task = this.activitiRules.getTaskService().createTaskQuery().singleResult();
 		assertEquals("Write monthly financial report", task.getName());
-		this.activitiRule.getTaskService().complete(task.getId());
-		assertEquals(this.activitiRule.getTaskService().createTaskQuery().singleResult().getName(),
+		this.activitiRules.getTaskService().complete(task.getId());
+		assertEquals(this.activitiRules.getTaskService().createTaskQuery().singleResult().getName(),
 				"Verify monthly financial report");
 	}
 
 	@Test
 	@Deployment(resources = { "MyProcess2.bpmn20.xml" })
 	public void myProcess2Test() {
-		ProcessInstance processInstance = this.activitiRule.getRuntimeService()
+		ProcessInstance processInstance = this.activitiRules.getRuntimeService()
 				.startProcessInstanceByKey("financialReport");
 		assertNotNull(processInstance);
-		Task task = this.activitiRule.getTaskService().createTaskQuery().singleResult();
+		Task task = this.activitiRules.getTaskService().createTaskQuery().singleResult();
 		assertEquals("Write monthly financial report", task.getName());
-		this.activitiRule.getTaskService().complete(task.getId());
-		TaskQuery taskQuery = this.activitiRule.getTaskService().createTaskQuery().active();
+		this.activitiRules.getTaskService().complete(task.getId());
+		TaskQuery taskQuery = this.activitiRules.getTaskService().createTaskQuery().active();
 		List<Task> list = taskQuery.list();
 		assertEquals(list.get(0).getName(), "Verify monthly financial report");
 		assertEquals(list.get(1).getName(), "DoStuff");
